c58774498831a818261bea57e1c7d5d55acfc4ac
mateus c <mateus.mcg@gmail.com>
2020-12-10 19:10:50 -0300

    Initial Firebase setup with Firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireAuthGuardModule } from '@angular/fire/auth-guard';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireAuthGuardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCyORM5OR1lghXa6xpTuEfWPEiv50ymfuA",
+    authDomain: "ragnarok-builds.firebaseapp.com",
+    projectId: "ragnarok-builds",
+    storageBucket: "ragnarok-builds.appspot.com",
+    messagingSenderId: "863593583042",
+    appId: "1:863593583042:web:9a73fbadf0e463b8c9865b",
+    measurementId: "G-GTLE5DVENK"
+  }
 };
